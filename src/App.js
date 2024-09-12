af5eedbb7f84f10db49bffd485ad2e3002e9dcb0
Tajeddine Bourhim <96800858+scorpionTaj@users.noreply.github.com>
2024-09-12 18:03:17 +0100

    Update

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import react, {useState} from 'react';
 import axios from 'axios';
 import './App.css';
 
+require('dotenv').config();
+
 function App() {
     const [image, setImage] = useState('')
 
     const handleChange = () => {
         axios
-            .get('https://api.generated.photos/api/v1/faces?api_key=S8GHTpwfr-rgO5fkEUnTew&order_by=random')
+            .get(`https://api.generated.photos/api/v1/faces?api_key=${process.env.API_GENERATED_PHOTOS}&order_by=random`)
             .then(res => {
             const url = res.data.faces[0].urls[4][512];
             url && setImage(url);
